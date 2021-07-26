35f95b7167864b83865be23a1adfae9b414c65d2
vukicn <60354694+vukicn@users.noreply.github.com>
2021-07-26 11:45:17 -0700

    intital commit

---
+++ demo1.java
@@ -0,0 +1,10 @@
+public class demo1{
+  public static void main(String[] args) {
+
+    int a =10;
+    System.out.println("value: "+ a);
+    System.out.println("First java program on Atiom IDE...");
+    
+
+  }
+}
